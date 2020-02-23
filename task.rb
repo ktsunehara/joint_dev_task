3b77e5ba1142b0ee87092f849c024df01e09b4cc
常原清隆 <tsuneharakiyotaka@tsuneharakiyotakanoMacBook-Air.local>
2020-02-23 13:05:21 +0900

    ruby kadai 1~7

--- task.rb
+++ task.rb
@@ -5,6 +5,7 @@ def q1
   names = ["田中", "佐藤", "佐々木", "高橋"]
 
   # 以下に回答を記載
+  p names.push("斎藤")
 
 end
 
@@ -13,6 +14,7 @@ def q2
   array2 = %w(bird bat tiger)
 
   # 以下に回答を記載
+  p array = array1 + array2
 
 end
 
@@ -20,6 +22,8 @@ def q3
   numbers = [1, 5, 8, 10, 2, 3, 2, 3, 3, 1, 4, 5, 9]
 
   # 以下に回答を記載
+  puts numbers.count(3)
+
 
 end
 
@@ -27,6 +31,7 @@ def q4
   sports = ["サッカー", "フットサル", nil, "野球", "バスケ", nil, "バレー"]
 
   # 以下に回答を記載
+  p sports.compact!
 
 end
 
@@ -35,6 +40,8 @@ def q5
   array2 = [1, 5, 8, 10]
 
   # 以下に回答を記載
+  p array1.empty?
+  p array2.empty?
 
 end
 
@@ -42,6 +49,7 @@ def q6
   numbers1 = [1, 2, 3, 4, 5]
 
   # 以下に回答を記載
+  p numbers2 = numbers1.map{ |n| n *10}
 
 end
 
@@ -49,6 +57,8 @@ def q7
   array = ["1", "2", "3", "4", "5"]
 
   # 以下に回答を記載
+  array = ["1", "2", "3", "4", "5"].map(&:to_i)
+  p array
 
 end
 
